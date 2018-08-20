95cce93806b09f98dc4a3f4c430b19048702e141
Nick iloba <nick.iloba@mullenlowe.com>
2018-08-20 15:50:42 -0400

    using map for list

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Nick', age: '30', belt: 'red', id: '1' },
+      { name: 'Efe', age: '27', belt: 'blue', id: '2' },
+      { name: 'Toby', age: '28', belt: 'black', id: '3' }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react app</h1>
         <p>Welcome :-)</p>
-        <Ninjas name="nick" age="30" belt="black" />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 
 class Ninjas extends Component {
     render() {
+        const { ninjas } = this.props;
+        const ninjasList = ninjas.map(ninja => {
+            return (
+                <div className="Ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            )
+        })
         return (
-            <div className="Ninja">
-                <div>Name: {this.props.name}</div>
-                <div>Age: {this.props.age}</div>
-                <div>Belt: {this.props.belt}</div>
+            <div className="ninja-list">
+                {ninjasList}
             </div>
         );
     }
